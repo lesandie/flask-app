88299f0e1619bbc1059146319e8406863cc47c1e
Diego Nieto <dnieto@tinybird.co>
2021-11-17 10:36:25 +0100

    Fixed tests

---
+++ tests/env.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#Listening Port
+export PORT=6000
+#Postgres connection vars
+export PGDB=test
+export PGUSER=test
+export PGPASS=test
+export PGPORT=5432
+export PGHOST=192.168.11.171
--- tests/tests.py
+++ tests/tests.py
@@ -1,16 +1,17 @@
 import sys
-sys.path.append("/home/dnieto/Repos/revolut/app")
+sys.path.append("/home/dnieto/Repos/revolut/hello")
 import requests
 import json
-import helloapp
+import os
+from helloapp import *
 from database import *
 
 db_params = {
-        'db': 'test',
-        'user': 'test',
-        'pass': 'test',
-        'host': 'localhost',
-        'port': '5432'
+        'db': os.environ.get("PGDB"),
+        'user': os.environ.get("PGUSER"),
+        'pass': os.environ.get("PGPASS"),
+        'host': os.environ.get("PGHOST"),
+        'port': os.environ.get("PGPORT")
     }
 
 def test_database():
@@ -33,7 +34,7 @@ def test_add_username():
     #HTTP methods in the requests library have a json parameter that will perform json.dumps() 
     #and set the Content-Type header to application/json:
     # Instead of response = requests.put(url, data=data, headers=headers)
-    url = f"http://localhost:8080/hello/test"
+    url = f"""http://localhost:{os.environ.get("PORT")}/hello/test"""
     # send PUT request
     response = requests.put(url, json=data)
     if response.status_code != 204:
