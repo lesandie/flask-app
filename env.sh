8a6d1699ffbd5e360dad3a5fc34fdb4995e5aff9
Diego Nieto <dnieto@tinybird.co>
2021-11-21 21:32:37 +0100

    Some minor fixes

--- env.sh
+++ env.sh
@@ -2,8 +2,8 @@
 #Listening Port
 export PORT=6000
 #Postgres connection vars
-export POSTGRES_DB=test
+export POSTGRES_DB=tests
 export POSTGRES_USER=test
 export POSTGRES_PASSWORD=test
 export POSTGRES_PORT=5432
-export POSTGRES_HOST=192.168.11.171
+export POSTGRES_HOST=192.168.11.149
