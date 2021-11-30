6f9b6c7b3bcb188b6c734e7948fa357aba8e2b24
Diego Nieto (lesandie) <lesandie@gmail.com>
2021-11-30 11:53:23 +0100

    removed database.py and integrated the funcs in helloapp

--- hello/helloapp.py
+++ hello/helloapp.py
@@ -4,11 +4,10 @@ Flask birthday app for Revolut
 """
 
 from flask import Flask, request, jsonify
-import sys, os
+import sys
+import os
+import psycopg2
 from datetime import datetime
-from database import *
-
-app = Flask(__name__)
 
 # DB params that will be used to connect to the db
 db_params = {
@@ -18,6 +17,53 @@ db_params = {
     'host': os.environ.get("POSTGRES_HOST"),
     'port': os.environ.get("POSTGRES_PORT")
     }
+
+def connect_postgres(db_params: dict):
+        """
+            Connect to PostgreSQL: db_params = {'host':, 'port':, 'user':, 'pass':, 'db':}
+            Returns a connection
+        """
+        try:
+            pg_pool = psycopg2.connect(f"dbname={db_params['db']} user={db_params['user']} password={db_params['pass']} host={db_params['host']} port={db_params['port']}")
+            if(pg_pool):
+                print("PostgreSQL connection created")
+                return pg_pool
+        except(Exception, psycopg2.DatabaseError) as error:
+            print(f"Error while connecting to PostgreSQL: {error}")
+            return False
+
+def put_data(username, bday, db_params: dict):
+    """
+        Puts data (username and birthdate into postgres) 
+    """ 
+    query = f"INSERT INTO bday (username, birthday) VALUES ('{username}', to_timestamp('{bday}', 'YYYY-MM-DD'));"
+    conn = connect_postgres(db_params)
+    with conn.cursor() as cursor:
+        cursor.execute(query)
+        # Important to commit with any UPSERT/DELETE
+        conn.commit()
+        print("PostgreSQL connection closed")
+
+def get_data(username, db_params: dict):
+    """
+        Returns the birthdate for a given username
+    """
+    query = f"SELECT to_char(birthday, 'YYYY-MM-DD') AS bday FROM bday WHERE username = '{username}';"
+    conn = connect_postgres(db_params)
+    with conn.cursor() as cursor:
+        cursor.execute(query)
+        # If no results then return None
+        if cursor.rowcount == 0:
+            print("PostgreSQL connection closed")
+            return None
+        else:
+            # Fetch the first row (we could use here LIMIT 1)
+            data = cursor.fetchone()
+            print("PostgreSQL connection closed")
+            return data[0]
+
+### Flask app begin
+app = Flask(__name__)
 
 # test endpoint
 @app.route('/')
